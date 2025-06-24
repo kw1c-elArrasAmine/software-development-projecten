6102d40634a9b272538defb14e5c553dc00a398c
kw1c-elArrasAmine <159121605+kw1c-elArrasAmine@users.noreply.github.com>
2025-06-24 21:49:00 +0200

    Add files via upload
    Gezamelijke website, waarbij pokemon de onderwerp is. Gebruik gemaakt van html css en javascript.

---
+++ Pokemon_website/scripts/home.js
@@ -0,0 +1,8 @@
+/*
+Auteurs: Amine, Abel, Jort
+Datum: 10-10-2024
+*/
+var naam = "Wat is je naam?";
+
+var jouwnaam = prompt(naam);
+naam = document.getElementById("name").innerHTML = "Welkom " + jouwnaam + " bij de Pokémon homepagina.";
